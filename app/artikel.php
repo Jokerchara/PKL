67c582d5fc3866db6be12af982d65489765dc37e
Albi Nuryachya <taegds@gmail.com>
2019-07-18 20:45:09 -0600

    Update

--- app/artikel.php
+++ app/artikel.php
@@ -7,24 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Artikel extends Model
 {
     protected $fillable = [
-        'judul','slug','foto',
-        'konten','id_user','id_catagory'
+        'judul', 'slug', 'foto',
+        'konten', 'id_user', 'id_catagory'
     ];
     public $timestamps = true;
 
     public function catagory()
     {
-        return $this->belongsTo('App\catagory','id_catagory');
+        return $this->belongsTo('App\catagory', 'id_catagory');
     }
 
     public function user()
     {
-        return $this->belongsTo('App\User','id_user');
+        return $this->belongsTo('App\User', 'id_user');
     }
 
     public function tag()
     {
-        return $this->belongsToMany('App\Tag','artikel_tag','id_artikel','id_tag');
+        return $this->belongsToMany('App\tag', 'artikel_tag', 'id_artikel', 'id_tag');
     }
 
 
@@ -33,4 +33,3 @@ class Artikel extends Model
         return 'slug';
     }
 }
-
